fd1c640a87e471015e878695c00eb3f7f0a1ae89
jianzhonghe@outlook.com <jianzhonghe@outlook.com>
2017-09-25 14:19:26 +0800

    merge the sync_bn_layer to the PSPNet

--- include/caffe/common.cuh
+++ include/caffe/common.cuh
@@ -6,7 +6,10 @@
 #include <cuda.h>
 
 // CUDA: atomicAdd is not defined for doubles
-static __inline__ __device__ double atomicAdd(double *address, double val) {
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+static __inline__ __device__ double atomicAdd(double *address, double val)
+{
   unsigned long long int* address_as_ull = (unsigned long long int*)address;
   unsigned long long int old = *address_as_ull, assumed;
   if (val==0.0)
@@ -17,5 +20,6 @@ static __inline__ __device__ double atomicAdd(double *address, double val) {
   } while (assumed != old);
   return __longlong_as_double(old);
 }
+#endif
 
 #endif
---
+++ include/caffe/layers/sync_bn_layer.hpp
@@ -0,0 +1,56 @@
+#ifndef CAFFE_SYNC_BN_LAYER_HPP_
+#define CAFFE_SYNC_BN_LAYER_HPP_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#ifdef USE_CUDNN
+#include "caffe/util/cudnn.hpp"
+#endif
+namespace caffe{
+#ifdef USE_MPI
+template <typename Dtype>
+class SyncBNLayer : public Layer<Dtype> {
+ public:
+  explicit SyncBNLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "SyncBN"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Dtype bn_momentum_;
+  Dtype bn_eps_;
+
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+
+  Blob<Dtype> mean_buffer_;
+  Blob<Dtype> var_buffer_;
+};
+#endif
+}// namespace caffe
+#endif
